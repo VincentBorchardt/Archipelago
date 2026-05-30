639e4bbf990f815568c31f6ba1a12801d81d2696
VincentBorchardt <vborchardt123@gmail.com>
2026-05-30 14:30:31 -0500

    Moved async_server into Civ4Context
    Actually trying to connect/log into the multiworld now, and while it kinda works, something's wrong and not letting me complete the login (and I don't think it's the server's fault in particular)

--- worlds/civ_4/client/server.py
+++ worlds/civ_4/client/server.py
@@ -17,70 +17,41 @@ class Civ4Context(CommonContext):
     game = "Civilization IV"
     items_handling = 0b111  # full remote
 
-    communication_task = None
+    #communication_task = None
 
     async def civ4_loop(self):
         while not self.exit_event.is_set():
             pass
 
-
-async def server_program_noninteractive():
-    # THIS NEEDS TO BE ITS OWN THING
-    print("starting server program")
-
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
-
-    print(host)
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = "This is a non-interactive test of stuff"
-        conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
-
-async def async_server(reader, writer):
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = await reader.read(1024)
-        data = data.decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        new_data = "This is a non-interactive test of stuff".encode()
-        #print(new_data)
-        writer.write(new_data)  # send data to the client
-        await writer.drain()  # Flow control, see later
-
+    async def async_server(self, reader, writer):
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = await reader.read(1024)
+            data = data.decode()
+            if not data:
+                # if data is not received break
+                break
+            login_info = data.split(';')
+            self.server_address = login_info[0]
+            self.auth = login_info[1]
+            self.password = login_info[2]
+            self.server_task = asyncio.create_task(server_loop(self), name="server loop")
+            print("from connected user: " + str(login_info))
+            new_data = "This is a non-interactive test of stuff".encode()
+            writer.write(new_data)  # send data to the client
+            await writer.drain()  # Flow control, see later
 
 # DELETED 'args: Namespace' FROM THIS SINCE IT WOULDN'T RUN
 async def main() -> None:
 
     ctx = Civ4Context()
-    ctx.server_task = asyncio.create_task(server_loop(ctx), name="server loop")
+    #ctx.server_task = asyncio.create_task(server_loop(ctx), name="server loop")
 
     if gui_enabled:
         ctx.run_gui()
     ctx.run_cli()
 
     # CLIENT LOOP STUFF GOES HERE
-    # Presumably this needs to be replaced with a task something or other
 
     # get the hostname
     host = socket.gethostname()
@@ -88,13 +59,11 @@ async def main() -> None:
 
     #ctx.communication_task = asyncio.create_task(server_program_noninteractive(), name="communication loop")
 
-    server = await asyncio.start_server(async_server, host, port)
+    server = await asyncio.start_server(ctx.async_server, host, port)
     await server.start_serving()
 
     await ctx.exit_event.wait()
     await ctx.shutdown()
 
 if __name__ == '__main__':
-    #server_program()
-
     asyncio.run(main())
