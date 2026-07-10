13b6602d30a559b22d2123d6e0a841f8094e8169
VincentBorchardt <vborchardt123@gmail.com>
2026-07-10 00:19:07 -0500

    Added lists of techs by era
    Somehow I had to fight with the AI to even do something this simple, but I eventually got it--next comes the rules for prerequisites, which is going to be a pain (and something I don't even want to let the AI try to do).

--- worlds/civ_4/regions.py
+++ worlds/civ_4/regions.py
@@ -7,6 +7,120 @@ from BaseClasses import Entrance, Region
 if TYPE_CHECKING:
     from .world import Civ4World
 
+ANCIENT_TECHS = [
+    "The Wheel",
+    "Agriculture",
+    "Animal Husbandry",
+    "Fishing",
+    "Hunting",
+    "Mysticism",
+    "Archery",
+    "Pottery",
+    "Writing",
+    "Sailing",
+    "Masonry",
+    "Mining",
+    "Priesthood",
+    "Bronze Working",
+    "Polytheism",
+    "Meditation",
+    "Monotheism",
+]
+
+CLASSICAL_TECHS = [
+    "Calendar",
+    "Monarchy",
+    "Alphabet",
+    "Mathematics",
+    "Construction",
+    "Code of Laws",
+    "Metal Casting",
+    "Compass",
+    "Currency",
+    "Horseback Riding",
+    "Iron Working",
+    "Literature",
+    "Aesthetics",
+    "Drama",
+]
+
+MEDIEVAL_TECHS = [
+    "Feudalism",
+    "Machinery",
+    "Civil Service",
+    "Guilds",
+    "Philosophy",
+    "Optics",
+    "Theology",
+    "Paper",
+    "Music",
+    "Divine Right",
+    "Banking",
+    "Engineering"
+]
+
+RENAISSANCE_TECHS = [
+    "Economics",
+    "Constitution",
+    "Astronomy",
+    "Democracy",
+    "Education",
+    "Chemistry",
+    "Corporation",
+    "Replaceable Parts",
+    "Gunpowder",
+    "Rifling",
+    "Printing Press",
+    "Nationalism",
+    "Military Science",
+    "Military Tradition",
+    "Liberalism"
+]
+
+INDUSTRIAL_TECHS = [
+    "Railroad",
+    "Electricity",
+    "Assembly Line",
+    "Steel",
+    "Medicine",
+    "Industrialism",
+    "Communism",
+    "Scientific Method",
+    "Steam Power",
+    "Fission",
+    "Combustion",
+    "Biology",
+    "Physics",
+    "Fascism",
+    "Artillery"
+]
+
+MODERN_TECHS = [
+    "Radio",
+    "Flight",
+    "Mass Media",
+    "Plastics",
+    "Computers",
+    "Ecology",
+    "Refrigeration",
+    "Rocketry",
+    "Robotics",
+    "Satellites",
+    "Laser",
+    "Fiber Optics",
+    "Advanced Flight",
+    "Superconductors",
+    "Composites"
+]
+
+FUTURE_TECHS = [
+    "Fusion",
+    "Genetics",
+    "Stealth",
+    "Future Tech"
+]
+
+
 
 def create_and_connect_regions(world: Civ4World) -> None:
     create_all_regions(world)
